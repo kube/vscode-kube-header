3048e8a2a92a67dea51424c587c8aa36202a972e
Christophe Feijoo <christophe@feijoo.fr>
2016-09-12 01:22:50 +0200

    First version

---
+++ src/extension.ts
@@ -0,0 +1,27 @@
+'use strict'
+import * as vscode from 'vscode'
+import { ExtensionContext, TextEdit, Position } from 'vscode'
+import { getHeader } from './header'
+
+export function activate(context: vscode.ExtensionContext) {
+
+  let disposable = vscode.commands
+    .registerCommand('extension.insertHeader', () => {
+      let activeTextEditor = vscode.window.activeTextEditor
+
+      activeTextEditor.edit(editor => {
+        let languageHeader = getHeader(activeTextEditor.document.languageId)
+
+        if (languageHeader)
+          editor.insert(new Position(0, 0), languageHeader)
+        else
+          vscode.window.showInformationMessage(
+            'No header found for current file language')
+      })
+    })
+
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {
+}
---
+++ src/header.ts
@@ -0,0 +1,100 @@
+const hashHeader =
+  `
+      #########.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##."
+
+`
+
+const slashHeader =
+  `
+      /*#######.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##*/
+
+`
+
+const semicolonHeader =
+  `
+      ;;;;;;;;;.
+     ;;;;;;;;',;:
+   ;;;;;;;;;',;;".
+  ;;';;';; .;;',;;.
+   ;; ;; ;; ; ;;",;.
+    ;; ;; ;; ;; ;;'
+     ;; ;; ;; :;;
+      ;; ;; ;;.'
+
+`
+
+const parensHeader =
+  `
+      (*#######.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##*)
+
+`
+
+const percentHeader =
+  `
+      %%%%%%%%%.
+     %%%%%%%%",%:
+   %%%%%%%%%',%%".
+  %%'%%'%% .%%',%%.
+   %% %% %% % %%",%.
+    %% %% %% %% %%'
+     %% %% %% :%%
+      %% %% %%."
+
+`
+
+const headers = {
+  'c': slashHeader,
+  'coffeescript': hashHeader,
+  'cpp': slashHeader,
+  'css': slashHeader,
+  'dockerfile': hashHeader,
+  'fsharp': parensHeader,
+  'go': hashHeader,
+  'groovy': slashHeader,
+  'jade': slashHeader,
+  'javascript': slashHeader,
+  'javascriptreact': slashHeader,
+  'less': slashHeader,
+  'makefile': hashHeader,
+  'objective-c': slashHeader,
+  'perl': hashHeader,
+  'perl6': hashHeader,
+  'php': slashHeader,
+  'plaintext': hashHeader,
+  'powershell': hashHeader,
+  'python': hashHeader,
+  'r': hashHeader,
+  'ruby': hashHeader,
+  'rust': slashHeader,
+  'scss': slashHeader,
+  'shellscript': hashHeader,
+  'sql': hashHeader,
+  'swift': slashHeader,
+  'typescript': slashHeader,
+  'typescriptreact': slashHeader,
+  'xsl': slashHeader,
+  'yaml': hashHeader
+}
+
+export const getHeader = (language: string) => headers[language]
